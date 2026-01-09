0abbc2b013c109287530b3eff9c68d2eb9cae922
Diogo <fmlsp2015@gmail.com>
2026-01-09 15:45:25 -0300

    layout: add line breaks to method selection panel

--- main.go
+++ main.go
@@ -96,8 +96,10 @@ func (m model) View() string {
 	leftContent := lipgloss.JoinVertical(
 		lipgloss.Left,
 		methodList.String(),
+		"/n",
 		fmt.Sprintf("Selected Method: %s", m.methods[m.selectedMethod]),
 		m.input.View(),
+		"/n",
 		"↑ / ↓ to change • q to quit",
 	)
 	
